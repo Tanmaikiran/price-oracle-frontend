d098d842c69af642a719f5064a3caa9a7763ccee
Tanmaikiran <tanmaibattu@gmail.com>
2025-07-16 22:58:17 +0530

    Create pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: 20 }}>
+      <h1>Hello from Price Oracle Frontend ✅</h1>
+      <p>This site is deployed and working!</p>
+    </div>
+  );
+}
