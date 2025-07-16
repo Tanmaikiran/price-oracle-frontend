9b6817e8028c9ee79156ed3c92308eeae590bf2e
Tanmaikiran <tanmaibattu@gmail.com>
2025-07-16 22:27:32 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'Arial' }}>
+      <h1>🎯 Price Oracle Frontend</h1>
+      <p>This is your deployed frontend. We'll connect it to the backend next.</p>
+    </div>
+  );
+}
